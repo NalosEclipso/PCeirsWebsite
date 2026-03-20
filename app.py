9e0e21d1ea180a8bccacc090f943a6e962152903
NalosEclipso <140351423+NalosEclipso@users.noreply.github.com>
2026-03-20 05:22:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def testePáginaInicial():
-  return "<h1>FEZES</h1>"
+  return """<h1>Bem vindo ao site dos</h1>
+          <marquee>ESTELIONATÁRIOS</marquee>
+          <h1>do BRASIL</h1>"""
+
+@app.route('/usuario/<nome>')
+def PagUsuario(nome):
+  return f"Olá, {nome} estelionatário(a)"
