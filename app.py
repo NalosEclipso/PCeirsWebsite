ba1def116075187044dd8021361788ab49365278
Yzol <147116158+lzxv2@users.noreply.github.com>
2026-03-21 15:26:13 -0300

    Wrap marquee text in strong tags for emphasis

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def testePáginaInicial():
   return """<h1>Bem vindo ao site dos</h1>
-          <marquee>ESTELIONATÁRIOS</marquee>
+          <strong><marquee>ESTELIONATÁRIOS</marquee></strong>
           <h1>do BRASIL</h1>"""
 
 @app.route('/usuario/<nome>')
